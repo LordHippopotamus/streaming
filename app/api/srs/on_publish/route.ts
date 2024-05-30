8fdacf63080325daaf8f2b46090570987bd5343c
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2024-05-30 20:55:49 +0300

    feat: add drm with token

---
+++ app/api/srs/on_publish/route.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const streamToken = new URLSearchParams(body.param).get("token");
+  const streamKey = body.stream;
+
+  if (!streamKey || typeof streamKey !== "string" || !streamToken) {
+    return new NextResponse("malformed request", { status: 400 });
+  }
+
+  const prisma = new PrismaClient();
+  const res = await prisma.user.findFirst({ where: { id: streamKey } });
+
+  if (res?.streamToken !== streamToken) {
+    return new NextResponse("unauthorized", { status: 401 });
+  }
+
+  return NextResponse.json({ code: 0 });
+}
